e134528edb01f0517668e7c15fd9c1d638b193e0
Jade Figueredo <jadefigueredo@outlook.com>
2023-04-27 19:01:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dicionário de Mulheres Negras
+
+Dicionário que lista as mulheres negras que fizeram e fazem parte da construção da tecnologia no Brasil e no mundo.
+
+- Feito com CSS, HTML e JavaScript
+
+⚠️ Em construção
---
+++ script.js
@@ -0,0 +1,78 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+function carregaDicionario() {
+  var biografias = {
+    bio01: {
+      nome: "Valerie Laverne Thomas",
+      imagem: "https://primeirosnegros.b-cdn.net/wp-content/uploads/2021/10/topo-fonte-nasa-1138x1536.jpg",
+      descricao:"Em 1970, foi designada para supervisionar a criação do programa Landsat – primeiro satélite da NASA capaz de tirar fotos da Terra. Sua tarefa era desenvolver um programa capaz de traduzir e mapear os dados brutos e complexos do equipamento em padrões simples para que os cientistas pudessem tanto usá-los como estudá-los. Antes de se aposentar em 1995, a física criou um programa que permitiu aos cientistas estudar o cometa Halley, a camada de ozônio e apoiar a criação da espaçonave Voyager com sondas robóticas.",
+      fonte: "https://primeirosnegros.com/valerie-thomas-os-espelhos-a-ilusao-e-o-3d/"
+    },
+
+    bio02: {
+      nome: "Mary Jackson",
+      imagem:"https://super.abril.com.br/wp-content/uploads/2020/06/26-06_maryJackson_SITE.jpg?quality=90&strip=info&w=1024&h=682&crop=1",
+      descricao: "Mary Jackson se formou em 1942 e trabalhou por décadas como engenheira aeroespacial na NASA, Mary Jackson se formou em matemática e física e após algumas experiências fora da área por falta de oportunidades devido ao apartheid ela conseguiu uma vaga no Langley Research Center e trabalhava numa área segregada em que os cálculos de engenharia aeronáutica hoje feitos por computador, eram realizados por mentes humanas. Em 1958 ela se tornou a primeira mulher negra com o cargo de engenheira da NASA.",
+      fonte: "https://super.abril.com.br/historia/quem-foi-mary-jackson-a-cientista-negra-cujo-nome-batizara-a-sede-da-nasa/"
+    },
+
+    bio03: {
+      nome: "alguém aqui",
+      imagem:"http://placekitten.com/200",
+      descricao:"Informações sobre ela aqui",
+      fonte: " "
+    },
+
+    bio04: {
+      nome: "alguém aqui",
+      imagem:"http://placekitten.com/200",
+      descricao:"Informações sobre ela aqui" ,
+      fonte: " " 
+    },
+  };
+
+  var content = document.getElementById("content");
+
+  for (var bio in biografias) {
+
+    content.innerHTML +=
+
+      '<div class="card">' +
+
+      '<img src="' +
+
+      biografias[bio].imagem +
+
+      '"/>' +
+
+      "<details>" +
+
+      "<summary>" +
+
+      biografias[bio].nome +
+
+      "</summary>" +
+
+      "<p>" +
+
+      biografias[bio].descricao +
+
+      "</p>" +
+
+      '<a href="' +
+
+      biografias[bio].fonte +
+
+      '">Fonte</a>' +
+      
+      "</details></div>" 
+
+  }
+}
+
+carregaDicionario()
